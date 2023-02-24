acd4f44eb0c3be21ee35c52468ef550d562ef06c
MossyPebble <james200053@gmail.com>
2023-02-24 15:39:11 +0900

    다중 선형 회귀

---
+++ multiple.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+import joblib
+
+LABEL_COLUMN = 'answer'
+LABELS = [0, 1]
+
+def get_dataset(file_path, **kwargs):
+  dataset = tf.data.experimental.make_csv_dataset(
+      file_path,
+      batch_size=5, # Artificially small to make examples easier to show.
+      label_name=LABEL_COLUMN,
+      na_value="?",
+      num_epochs=1,
+      ignore_errors=True, 
+      **kwargs)
+  return dataset
+
+f = pd.read_csv('data.csv')
+print(f.head())
+
+x = f[['Tox', 'muns', 'Vgs', 'Vt', 'm', 'Vds']]
+y = f[['answer']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+mlr = LinearRegression()
+mlr.fit(x_train, y_train)
+joblib.dump(mlr, 'mlrmodel.pkl')
+
+# mlr = joblib.load('mlrmodel.pkl')
+
+y_predict = mlr.predict(x_test)
+
+plt.scatter(y_test, y_predict, alpha=0.4)
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.title("MULTIPLE LINEAR REGRESSION")
+plt.show()
+
+print(mlr.score(x_train, y_train))
